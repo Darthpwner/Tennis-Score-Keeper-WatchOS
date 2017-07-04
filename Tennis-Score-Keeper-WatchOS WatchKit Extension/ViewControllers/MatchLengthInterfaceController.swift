9645ec07bacdca06b6d3622fffb7a8d52f6a5a30
Darthpwner <matthewallenlin@gmail.com>
2017-07-04 00:00:20 -0700

    Set up basic logic

---
+++ Tennis-Score-Keeper-WatchOS WatchKit Extension/ViewControllers/MatchLengthInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  MatchLengthInterfaceController.swift
+//  Tennis-Score-Keeper-WatchOS
+//
+//  Created by Matthew Allen Lin on 7/3/17.
+//  Copyright © 2017 Matthew Allen Lin Software. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class MatchLengthInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
