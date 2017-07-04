1d3b8de444c6a95e0c9ae59d125a662d4b736858
Darthpwner <matthewallenlin@gmail.com>
2017-07-04 01:36:15 -0700

    Game score functionality works

--- Tennis-Score-Keeper-WatchOS WatchKit Extension/ViewControllers/ScoresInterfaceController.swift
+++ Tennis-Score-Keeper-WatchOS WatchKit Extension/ViewControllers/ScoresInterfaceController.swift
@@ -12,21 +12,30 @@ import Foundation
 
 class ScoresInterfaceController: WKInterfaceController {
 
-    var player_1_points = 0
-    var player_2_points = 0
+    // Game score values
+    var player_1_points_won_this_game = 0
+    var player_2_points_won_this_game = 0
+    
+    // Set score values
+    var player_1_set_1_score = 0
+    var player_2_set_1_score = 0
+    var player_1_set_2_score = 0
+    var player_2_set_2_score = 0
+    var player_1_set_3_score = 0
+    var player_2_set_3_score = 0
     
     //Outlets
     /* Set Scores */
-    @IBOutlet var player_1_set_1_score: WKInterfaceLabel!
-    @IBOutlet var player_2_set_1_score: WKInterfaceLabel!
-    @IBOutlet var player_1_set_2_score: WKInterfaceLabel!
-    @IBOutlet var player_2_set_2_score: WKInterfaceLabel!
-    @IBOutlet var player_1_set_3_score: WKInterfaceLabel!
-    @IBOutlet var player_2_set_3_score: WKInterfaceLabel!
+    @IBOutlet var player_1_set_1_score_label: WKInterfaceLabel!
+    @IBOutlet var player_2_set_1_score_label: WKInterfaceLabel!
+    @IBOutlet var player_1_set_2_score_label: WKInterfaceLabel!
+    @IBOutlet var player_2_set_2_score_label: WKInterfaceLabel!
+    @IBOutlet var player_1_set_3_score_label: WKInterfaceLabel!
+    @IBOutlet var player_2_set_3_score_label: WKInterfaceLabel!
     
     /* Game Scores */
-    @IBOutlet var player_1_game_score: WKInterfaceLabel!
-    @IBOutlet var player_2_game_score: WKInterfaceLabel!
+    @IBOutlet var player_1_game_score_label: WKInterfaceLabel!
+    @IBOutlet var player_2_game_score_label: WKInterfaceLabel!
     
     /* Player Serving Images */
     @IBOutlet var player_1_serving_image: WKInterfaceImage!
@@ -34,11 +43,63 @@ class ScoresInterfaceController: WKInterfaceController {
     
     /* Increment Scores */
     @IBAction func incrementPlayerOneScore() {
-        player_1_game_score.setText("15")
+        if(player_1_points_won_this_game >= 3 && player_1_points_won_this_game - player_2_points_won_this_game >= 1) {    //Game: Player 1
+            player_1_game_score_label.setText("0")
+            player_2_game_score_label.setText("0")
+            player_1_points_won_this_game = 0
+            player_2_points_won_this_game = 0
+            
+            print("GAME P1")
+            print(player_1_points_won_this_game)
+            print(player_2_points_won_this_game)
+            
+            return
+        } else if(player_1_points_won_this_game == 0) {
+            player_1_game_score_label.setText("15")
+        } else if(player_1_points_won_this_game == 1) {
+            player_1_game_score_label.setText("30")
+        } else if(player_1_points_won_this_game == 2) {
+            player_1_game_score_label.setText("40")
+        } else if(player_2_points_won_this_game - player_1_points_won_this_game == 0) {
+            player_1_game_score_label.setText("A")  //Won the deuce point
+        } else if(player_2_points_won_this_game - player_1_points_won_this_game == 1) {
+            player_2_game_score_label.setText("40") //Won the point on opponent's ad
+        }
+            
+        player_1_points_won_this_game += 1
+        
+        print(player_1_points_won_this_game)
+        print(player_2_points_won_this_game)
     }
     
     @IBAction func incrementPlayerTwoScore() {
-        player_2_game_score.setText("40")
+        if(player_2_points_won_this_game >= 3 && player_2_points_won_this_game - player_1_points_won_this_game >= 1) {    //Game: Player 2
+            player_1_game_score_label.setText("0")
+            player_2_game_score_label.setText("0")
+            player_1_points_won_this_game = 0
+            player_2_points_won_this_game = 0
+            
+            print("GAME P2")
+            print(player_1_points_won_this_game)
+            print(player_2_points_won_this_game)
+            
+            return
+        } else if(player_2_points_won_this_game == 0) {
+            player_2_game_score_label.setText("15")
+        } else if(player_2_points_won_this_game == 1) {
+            player_2_game_score_label.setText("30")
+        } else if(player_2_points_won_this_game == 2) {
+            player_2_game_score_label.setText("40")
+        } else if(player_1_points_won_this_game - player_2_points_won_this_game == 0) {
+            player_2_game_score_label.setText("A")  //Won the deuce point
+        } else if(player_1_points_won_this_game - player_2_points_won_this_game == 1) {
+            player_1_game_score_label.setText("40") //Won the point on opponent's ad
+        }
+        
+        player_2_points_won_this_game += 1
+        
+        print(player_1_points_won_this_game)
+        print(player_2_points_won_this_game)
     }
     
     
