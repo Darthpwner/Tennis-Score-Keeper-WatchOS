61fb524672f19a425649aa255cd52b0f3710a2e7
Darthpwner <matthewallenlin@gmail.com>
2017-07-03 22:53:19 -0700

    First commit

---
+++ Tennis-Score-Keeper-WatchOSTests/Tennis_Score_Keeper_WatchOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tennis_Score_Keeper_WatchOSTests.swift
+//  Tennis-Score-Keeper-WatchOSTests
+//
+//  Created by Matthew Allen Lin on 7/3/17.
+//  Copyright © 2017 Matthew Allen Lin Software. All rights reserved.
+//
+
+import XCTest
+@testable import Tennis_Score_Keeper_WatchOS
+
+class Tennis_Score_Keeper_WatchOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
